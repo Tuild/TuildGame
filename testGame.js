d03bdc201c5b0f9b5241f0cc6936147ee2f4a41d
Tuild <kedar132@gmail.com>
2017-03-14 10:51:22 -0700

    revert

--- testGame.js
+++ testGame.js
@@ -437,43 +437,17 @@ document.getElementById('vid').addEventListener('ended',myHandler,false);
 function calculateReflexes(){
 
     var imgToBeDisplayed = (noOfHits/noofBalls)*100;
-    imgToBeDisplayed = Math.floor(imgToBeDisplayed/10);
 
     if(imgToBeDisplayed>10){
 
-      // var str;
+      var text1 = "You Play Like The Best There is NELSON CRUZ";
+      var text2 = "You Are Almost There, You Play Just Like DAVID ORTIZ";
+      var text3 = "A Few More Hits And You Will Be Unbeatable Just Like EDWIN ENCARNACION"
+      var text4 = "Gotta Put In a Lot More Hours To Reach Your Maximum Potential like PRINCE FIELDER";
+      var text5 = "Still A Long Way To Go Just Like ALEX RODRIGUEZ";
 
-      // if(imgToBeDisplayed == 1){
-
-      //   str = "You Play Like The Best There is NELSON CRUZ";;
-
-      // }
-      // else if(imgToBeDisplayed==2){
-
-      //   str = "You Are Almost There, You Play Just Like DAVID ORTIZ";
-
-      // }
-      // else if(imgToBeDisplayed==3){
-
-      //   str = "A Few More Hits And You Will Be Unbeatable Just Like EDWIN ENCARNACION";
-        
-      // }
-      // else if(imgToBeDisplayed==4){
-
-      //   str = "Gotta Put In a Lot More Hours To Reach Your Maximum Potential like PRINCE FIELDER";;
-        
-      // }
-      // else {
-
-      //   str = "Still A Long Way To Go Just Like ALEX RODRIGUEZ";
-        
-      // }
-
-
-      
-
-      
-     
+      imgToBeDisplayed = Math.floor(imgToBeDisplayed/10);
+      var dispText = "text"+imgToBeDisplayed;
 
       videotag.style.display = "none";
       document.getElementById('QuestionBlock').style.display = 'none';
@@ -483,7 +457,7 @@ function calculateReflexes(){
 
       document.getElementById('results').style.display = 'block';
       document.getElementById('finalResult').src = imgToBeDisplayed;
-      // document.getElementById('dispText').innerHTML = str;
+      document.getElementById('dispText').innerHTML = dispText;
 
       
 
